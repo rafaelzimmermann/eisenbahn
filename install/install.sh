62ccbcc7ea859bb73e6d815dd422a96ad104a71a
Rafael Zimmermann <engzimmermann@gmail.com>
2019-01-09 20:02:01 +0100

    Update install script

--- install/install.sh
+++ install/install.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/rafaelzimmermann/eisenbahn.git /opt/eisenbahn
 
+cp /opt/eisenbahn/config.json.example /opt/eisenbahn/config.json
 cp /opt/eisenbahn/install/eisenbahn.service /etc/systemd/system/eisenbahn.service
 
 systemctl enable eisenbahn.service
