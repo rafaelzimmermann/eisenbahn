7eef2b20df93b50b0a486d47c4b251f82dc69ab2
Rafael Zimmermann <engzimmermann@gmail.com>
2019-01-15 21:24:26 +0100

    Fix special char U

--- lcd/special_chars.py
+++ lcd/special_chars.py
@@ -3,7 +3,7 @@
 def create_chars(lcd):
     degree_pattern = [6,9,9,6,0,0,0,0]
     celsius_pattern = [14,27,24,24,24,27,14,0]
-    u_pattern = [7,27,27,27,27,27,14,0]
+    u_pattern = [0,27,27,27,27,27,14,0]
     n0_pattern = [14,27,27,27,27,27,14,0]
     n1_pattern = [2,6,14,6,6,6,6,0]
     n7_pattern = [31,3,6,12,12,12,12,0]
